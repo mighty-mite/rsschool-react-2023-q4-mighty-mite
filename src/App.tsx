cc83a8f987a27301c57b412fad9754eb7fd4307e
Ayur Tushinov <tushinov.ayur@gmail.com>
2023-10-26 20:05:30 +0300

    fix: get rid of unnecessary files and folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,3 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-
-export function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-}
-
-export function WrappedApp() {
-  return (
-    <HashRouter>
-      <App />
-    </HashRouter>
-  );
+export default function App() {
+  return <div className="app">hi there</div>;
 }
