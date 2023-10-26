41dfc6c2583b79a0bbc4a97382bb4e46c1bac9a1
Ayur Tushinov <tushinov.ayur@gmail.com>
2023-10-26 22:00:04 +0300

    feat: add searchbar functionality

--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -1,11 +1,31 @@
+/* eslint-disable react/state-in-constructor */
 import { Component } from 'react';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class Searchbar extends Component {
+  state = {
+    text: '',
+  };
+
+  componentDidMount() {
+    this.setState({ text: localStorage.getItem('search') });
+  }
+
   render() {
+    const { text } = this.state;
+
     return (
       <form className="searchbar">
-        <input type="text" className="search-input" />
+        <input
+          value={text}
+          onChange={(e) =>
+            this.setState(() => {
+              localStorage.setItem('search', e.target.value);
+              return { text: e.target.value };
+            })
+          }
+          type="text"
+          className="search-input"
+        />
         <button type="submit">Search</button>
       </form>
     );
